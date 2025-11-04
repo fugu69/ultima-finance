3727ccab508fa15f099eb18a1939b1e62b0ce340
Maximus Brutalis <primeskobelev@gmail.com>
2025-11-04 15:09:06 +0700

    feat(auth): finish auth logic

--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
 from . import db
+from flask_login import UserMixin
 from datetime import datetime, timezone
 from sqlalchemy import func, extract
 from decimal import Decimal, ROUND_HALF_UP
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100))
