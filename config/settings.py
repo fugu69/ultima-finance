2c025db7291bf4d64cd90472dca04995c8d0d9c7
Maximus Brutalis <primeskobelev@gmail.com>
2026-06-23 16:08:39 +0700

    add bootstrap5 style to login and signup forms

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3d Party
+    'crispy_forms',
+    'crispy_bootstrap5',
+    # Local
     'finance',
     'accounts',
 ]
@@ -111,3 +115,6 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
