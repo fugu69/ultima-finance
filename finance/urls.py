15565ceed1da2427ee40ae2d47bdcb7940b0017d
Maximus Brutalis <primeskobelev@gmail.com>
2026-06-25 11:46:22 +0700

    feat(finance/url): change routing for dashboard and home destinations

--- finance/urls.py
+++ finance/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from .views import (
+    LandingPageView,
     HomePageView,
     SaleDetailView,
     SaleCreateView,
@@ -10,10 +11,15 @@ from .views import (
 )
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="home"),
-    path("sale/<int:pk>/", SaleDetailView.as_view(), name="sale_detail"),
-    path("sale/<int:sale_pk>/comment/", CommentCreateView.as_view(), name="comment_create"),
-    path("sale/create/", SaleCreateView.as_view(), name="sale_create"),
+    path(
+        "sale/<int:sale_pk>/comment/",
+        CommentCreateView.as_view(),
+        name="comment_create",
+    ),
     path("sale/<int:pk>/update/", SaleUpdateView.as_view(), name="sale_update"),
     path("sale/<int:pk>/delete/", SaleDeleteView.as_view(), name="sale_delete"),
+    path("sale/<int:pk>/", SaleDetailView.as_view(), name="sale_detail"),
+    path("sale/create/", SaleCreateView.as_view(), name="sale_create"),
+    path("dashboard/", LandingPageView.as_view(), name="dashboard"),
+    path("", HomePageView.as_view(), name="home"),
 ]
