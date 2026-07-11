a34aeb70a00fb4b756c416667f074721f4de785e
Maximus Brutalis <primeskobelev@gmail.com>
2026-07-11 16:38:53 +0700

    feat(finance/forms): create presentation comment form

--- finance/forms.py
+++ finance/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Comment
+from .models import Comment, PresentationComment
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
+        fields = ("comment",)
+
+class PresentationCommentForm(forms.ModelForm):
+    class Meta:
+        model = PresentationComment
         fields = ("comment",)
